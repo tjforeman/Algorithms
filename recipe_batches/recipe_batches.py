f7077d9df150d968d285b32cea166ced92934a51
tjforeman <tylerforeman92@gmail.com>
2019-11-06 18:45:31 -0500

    recipe batches completed

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,38 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  max_batches = []
+
+
+  for k,v in recipe.items():
+    print(f'{k}:{v}') 
+
+    if len(recipe) > len(ingredients):
+      return 0
+
+    if k in ingredients:
+      if recipe[k] <= ingredients[k]:
+
+        max_batches.append(ingredients[k] // recipe[k])
+        print(max_batches)
+        print(ingredients[k])
+        print(recipe[k])
+
+        return min(max_batches)
+
+      else:
+        return 0
+
+  return max_batches
+
+
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = {'milk': 2, 'sugar': 40, 'butter': 20 }
+  ingredients = {'milk': 5, 'sugar': 120, 'butter': 500 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+  # milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 }), 2)
